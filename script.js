2d06ffe0cd67e30d4610a315edad03111d6e35f1
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-09-04 22:28:44 -0300

    5.1

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const iniciarOuPausarBtIcone = document.querySelector('#start-pause img')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav')
+const audioPausa = new Audio('/sons/pause.mp3')
+const audioTempoFinalizado = new Audio('/sons/beep.mp3')
 
 
 let tempoDecorridoEmSegundos = 10
@@ -73,9 +78,10 @@ function alterarContexto (contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play();
+        //alert('Tempo finalizado!')
         zerar()
-        alert('Tempo finalizado!')
-        return
+        return tempoDecorridoEmSegundos = 10;
     }
     tempoDecorridoEmSegundos -= 1
     console.log('Temporizador: ' + tempoDecorridoEmSegundos)
@@ -85,13 +91,19 @@ startPauseBt.addEventListener('click', iniciarOuPausar)
 
 function iniciarOuPausar() {
     if(intervaloId) {
+        audioPausa.play();
         zerar()
         return
     }
+    audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = 'Pausar'
+    iniciarOuPausarBtIcone.setAttribute('src', '/imagens/pause.png')
 }
 
 function zerar() {
     clearInterval(intervaloId)
-    intervaloId = null
+    iniciarOuPausarBt.textContent = 'Começar'
+    iniciarOuPausarBtIcone.setAttribute('src', '/imagens/play_arrow.png')
+    intervaloId = null 
 }
